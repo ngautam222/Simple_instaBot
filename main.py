cd249f9e8ea30ba96de2049bc14927942cf0d2aa
Nikhil Gautam <nikhilgautam@Nikhils-MBP.cable.rcn.com>
2021-01-15 14:36:42 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from instapy import InstaPy
+
+session = InstaPy(username="YOUR Account", password="your password")
+session.login()
+session.like_by_tags(['travel', 'bitcoin', 'cricket', 'foodporn','foodlover', 'sports', 'fashion'], amount=60)
+session.set_do_follow(True, percentage=35)
+session.set_do_comment(True, percentage=50)
+session.set_comments(['nice post', 'wow', 'pretty cool dude'])
+session.end()
